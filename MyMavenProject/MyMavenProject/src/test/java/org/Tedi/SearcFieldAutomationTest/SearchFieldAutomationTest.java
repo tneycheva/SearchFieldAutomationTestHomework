bd1ece765e1a7d0810d4b16d8167bac45d13a788
tneycheva <119449269+tneycheva@users.noreply.github.com>
2023-02-01 13:56:21 +0200

    updating homework

--- MyMavenProject/MyMavenProject/src/test/java/org/Tedi/SearcFieldAutomationTest/SearchFieldAutomationTest.java
+++ MyMavenProject/MyMavenProject/src/test/java/org/Tedi/SearcFieldAutomationTest/SearchFieldAutomationTest.java
@@ -1,4 +1,4 @@
-package org.Tedi.SearcFieldAutomationTest;
+package org.Tedi.SearchFieldAutomationTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -11,12 +11,13 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+
 public class SearchFieldAutomationTest {
 
 
-
     @Test
-    public void testSearchField() {
+    public void testSearchFieldOnHomePage() {
+
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -53,8 +54,8 @@ public class SearchFieldAutomationTest {
 
         //Search field on Home page
 
-        WebElement searchField = driver.findElement(By.id("search-bar"));
-        searchField.sendKeys("MARIELKATA");
+        WebElement searchFieldOnHomePage = driver.findElement(By.id("search-bar"));
+        searchFieldOnHomePage.sendKeys("MARIELKATA");
 
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         WebElement userMarielkata = driver.findElement(By.partialLinkText("MARIELKATA"));
@@ -64,7 +65,47 @@ public class SearchFieldAutomationTest {
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/32"));
 
+        Boolean isTextDisplayed = wait.until(ExpectedConditions.textToBe(By.tagName("h2"), "MARIELKATA"));
+        Assert.assertTrue(isTextDisplayed, "The username is not displayed");
+
+        driver.close();
+
+    }
+    @Test
+    public void testSearchFieldOnProfilePage() {
+
         //Search field on Profile page
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+        WebElement login = driver.findElement(By.id("nav-link-login"));
+        login.click();
+
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/login"));
+
+
+        WebElement signInTitle = driver.findElement(By.xpath("//p[text()='Sign in']"));
+        wait.until(ExpectedConditions.visibilityOf(signInTitle));
+
+
+        WebElement userNameField = driver.findElement(By.id("defaultLoginFormUsername"));
+        userNameField.sendKeys("teddy");
+
+        WebElement passwordField = driver.findElement(By.id("defaultLoginFormPassword"));
+        passwordField.sendKeys("123456");
+
+        WebElement signButton = driver.findElement(By.id("sign-in-button"));
+        wait.until(ExpectedConditions.elementToBeClickable(signButton));
+        signButton.click();
 
         WebElement profileLink = driver.findElement(By.id("nav-link-profile"));
         profileLink.click();
@@ -75,7 +116,7 @@ public class SearchFieldAutomationTest {
         Assert.assertTrue(isTextDisplayed, "The username is not displayed");
 
         WebElement searchFieldOnProfilePage = driver.findElement(By.id("search-bar"));
-        searchField.sendKeys("MARIELKATA");
+        searchFieldOnProfilePage.sendKeys("MARIELKATA");
 
         WebElement userMarielkata1 = driver.findElement(By.partialLinkText("MARIELKATA"));
 
@@ -84,8 +125,48 @@ public class SearchFieldAutomationTest {
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/32"));
 
+        Boolean isTextDisplayedMarielkataProfile = wait.until(ExpectedConditions.textToBe(By.tagName("h2"), "MARIELKATA"));
+        Assert.assertTrue(isTextDisplayedMarielkataProfile, "The username is not displayed");
+
+        driver.close();
+
+
+    }
+
+    @Test
+    public void testSearchFieldOnNewPostPage() {
 
         //Search field on New post page
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+        WebElement login = driver.findElement(By.id("nav-link-login"));
+        login.click();
+
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/login"));
+
+
+        WebElement signInTitle = driver.findElement(By.xpath("//p[text()='Sign in']"));
+        wait.until(ExpectedConditions.visibilityOf(signInTitle));
+
+
+        WebElement userNameField = driver.findElement(By.id("defaultLoginFormUsername"));
+        userNameField.sendKeys("teddy");
+
+        WebElement passwordField = driver.findElement(By.id("defaultLoginFormPassword"));
+        passwordField.sendKeys("123456");
+
+        WebElement signButton = driver.findElement(By.id("sign-in-button"));
+        wait.until(ExpectedConditions.elementToBeClickable(signButton));
+        signButton.click();
 
         WebElement newPostLink = driver.findElement(By.id("nav-link-new-post"));
         newPostLink.click();
@@ -95,7 +176,7 @@ public class SearchFieldAutomationTest {
         Assert.assertTrue(isPostTitleDisplayed, "The New Post page is not displayed");
 
         WebElement searchFieldOnNewPostPage = driver.findElement(By.id("search-bar"));
-        searchField.sendKeys("MARIELKATA");
+        searchFieldOnNewPostPage.sendKeys("MARIELKATA");
 
         WebElement userMarielkata2 = driver.findElement(By.partialLinkText("MARIELKATA"));
 
@@ -104,8 +185,10 @@ public class SearchFieldAutomationTest {
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/32"));
 
-        driver.close();
+        Boolean isTextDisplayedMarielkataProfile = wait.until(ExpectedConditions.textToBe(By.tagName("h2"), "MARIELKATA"));
+        Assert.assertTrue(isTextDisplayedMarielkataProfile, "The username is not displayed");
 
+        driver.close();
 
     }
 }
